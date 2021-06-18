ab2fc5afb88f8ceee85d091dec61c7312345e7fc
lukostelloe <lukostelloe@gmail.com>
2021-06-18 11:32:45 +0200

    mouseover

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,25 +1,3 @@
-// When the user presses the ASDFGHJKL keys on the keyboard, the buttons corresponding to the
-// letters on the page become bigger and brighter, and the corresponding drum sound
-// sounded.
-
-// Key points to remember
-// 1. Keyboard events
-// 2. Play the sound
-// 3. Change your style
-
-// Staged decomposition
-// 1. Add a .keydown keyboard event listener
-// 2. Create a function that triggers the audio for the corresponding data-key.
-// 3. Add the .playing class to the activated .key div.
-// 4. Add an event handler to remove the style at the end of the transition
-
-// (cf: in
-// the css, the .key class contains a transition property of .0.07s.
-// How do I map keyboard keys to page buttons?
-// How to make sure that when the button is held down, a continuous drum sound may
-// be played immediately?
-// How to restore the page buttons?
-
 let myDivs = document.querySelectorAll(".key");
 
 document.onkeydown = function (e) {
